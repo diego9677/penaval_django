ce9c2082a8110059761fd67e3acb302fc328929a
diego9677 <dieguito9677@gmail.com>
2023-09-18 20:26:10 -0400

    solve

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'penaval_django',
-        script: '/home/diego/penaval_django/.env/bin/gunicorn /home/diego/penaval_django/config.wsgi',
+        script: '/home/diego/penaval_django/.env/bin/gunicorn --env DJANGO_SETTINGS_MODULE=config.settings config.wsgi',
         cron_restart: '* * * * *'
       }
     ]
