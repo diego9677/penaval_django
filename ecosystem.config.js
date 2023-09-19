fdd721c14d8b8a0b8426d242a688fdcfcea726ee
diego9677 <dieguito9677@gmail.com>
2023-09-19 12:03:47 -0400

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,17 @@ module.exports = {
     apps: [
       {
         name: 'penaval_django',
-        script: '/home/diego/penaval_django/.env/bin/gunicorn --env DJANGO_SETTINGS_MODULE=config.settings config.wsgi',
-        cron_restart: '0'
+        script: 'gunicorn --env DJANGO_SETTINGS_MODULE=config.settings config.wsgi',
+        cron_restart: '0',
+        merge_logs: true,
+        autorestart: true,
+        log_file: "logs/combined.outerr.log",
+        out_file: "logs/out.log",
+        error_file: "logs/err.log",
+        log_date_format : "YYYY-MM-DD HH:mm Z",
+        append_env_to_name: true,
+        watch: false,
+        max_memory_restart: '5G',
       }
     ]
   };
