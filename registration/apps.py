5e9334116f6a4f0fbc5c3df1bba132e6a966b7b4
diego9677 <dieguito9677@gmail.com>
2023-09-19 11:53:44 -0400

    refactor code

--- registration/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'registration'
-    verbose_name = 'Registro'
