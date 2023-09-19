d52e5b56189e6bf935c8f15aa7c6910f6014a625
diego9677 <dieguito9677@gmail.com>
2023-09-19 12:17:23 -0400

    refactor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,26 @@
 module.exports = {
     apps: [
+      // {
+      //   name: 'penaval_django',
+      //   script: 'gunicorn --env DJANGO_SETTINGS_MODULE=config.settings config.wsgi',
+      //   cron_restart: '0',
+      //   merge_logs: true,
+      //   autorestart: false,
+      //   log_file: "logs/combined.outerr.log",
+      //   out_file: "logs/out.log",
+      //   error_file: "logs/err.log",
+      //   log_date_format : "YYYY-MM-DD HH:mm Z",
+      //   append_env_to_name: true,
+      //   watch: false,
+      //   max_memory_restart: '5G',
+      // }
       {
         name: 'penaval_django',
-        script: 'gunicorn --env DJANGO_SETTINGS_MODULE=config.settings config.wsgi',
+        interpreter: 'python3',
+        cmd: 'gunicorn --env DJANGO_SETTINGS_MODULE=config.settings config.wsgi',
         cron_restart: '0',
+        autorestart: false,
         merge_logs: true,
-        autorestart: true,
         log_file: "logs/combined.outerr.log",
         out_file: "logs/out.log",
         error_file: "logs/err.log",
